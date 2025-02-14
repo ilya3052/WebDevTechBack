ed6be7bd05eda46dbad2b27e0ae667c9b510261a
ilya3052 <ipopadin5@gmail.com>
2025-02-14 12:06:59 +0300

    add: добавлена html разметка формы

--- app/index.php
+++ app/index.php
@@ -6,34 +6,67 @@
     <title>Document</title>
 </head>
 <body>
-    <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $name = trim($_POST['name'] ?? '');
-            $email = trim($_POST['email'] ?? '');
-            $csv_file = 'data.csv';
-            $dataRow = [$name, $email];
-
-            if (($file = fopen($csvFile, 'a'))) {
-                fputcsv($file, $dataRow);
-                fclose($file);
-                $message = 'Даныне успешно сохранены';
-            }
-            else {
-                $message = 'Ошибка при сохранении данных';
-            }
-        }
-    ?>
-    <h1>Введите данные</h1>
-    <?php if (!empty($message)): ?>
-        <p><?php echo $message; ?></p>
-    <?php endif; ?>
-    
-    <form action="form.php" method="POST">
-        <label for="name">Имя:</label>
-        <input type="text" name="name" id="name">
-        <label for="email">Email:</label>
-        <input type="email" name="email" id="email">
-        <input type="submit" value="Отправить">
-    </form>
+    <h1>Курьерская служба</h1>
+    <div class="container">
+      <div class="text">
+         Contact us Form
+      </div>
+      <form action="form.php" method="POST">
+         <div class="form-row">
+            <div class="input-data">
+            <label for="courier">ФИО курьера</label>
+            <div class="underline"></div>
+            <input type="text" name="name" id="name">
+            </div>
+            <div class="input-data">
+                <label for="client">ФИО клиента</label>
+                <div class="underline"></div>
+                <input type="text" name="client" id="client">
+            </div>
+         </div>
+         <div class="form-row">
+            <div class="input-data">
+                <label for="client_mail">Почта клиента</label>
+                <div class="underline"></div>
+                <input type="email" name="client_mail" id="client_mail">
+            </div>
+            <div class="input-data">
+                <label for="product">Товар</label>
+                <div class="underline"></div>
+                <input type="text" name="product" id="product">
+            </div>
+         </div>
+         <div class="form-row">
+            <div class="input-data">
+                <label for="product_price">Стоимость товара</label>
+                <div class="underline"></div>
+                <input type="number" name="product_price" id="product_price">
+            </div>
+            <div class="input-data">
+                <label for="address">Адрес доставки</label>
+                <div class="underline"></div>
+                <input type="text" name="address" id="address">
+            </div>
+         </div>
+         <div class="form-row">
+            <div class="input-data">
+                <label for="date">Дата доставки</label>
+                <div class="underline"></div>
+                <input type="date" name="date" id="date">
+            </div>
+            <div class="input-data">
+                <label for="delivery_price">Стоимость доставки</label>
+                <div class="underline"></div>
+                <input type="number" name="delivery_price" id="delivery_price">
+            </div>
+         </div>
+         <div class="form-row submit-btn">
+               <div class="input-data">
+                  <div class="inner"></div>
+                  <input type="submit" value="Отправить">
+               </div>
+            </div>
+      </form>
+      </div>
 </body>
 </html>
