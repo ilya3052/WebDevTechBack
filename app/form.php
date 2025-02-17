f13318e1820a22d86722635b8147c19b20708943
ilya3052 <ipopadin5@gmail.com>
2025-02-17 17:00:14 +0300

    ref: исправление отступов

--- app/form.php
+++ app/form.php
@@ -1,34 +1,34 @@
 <?php
-        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
-            $courier_name = trim($_POST['name'] ?? '');
-            $client_name = trim($_POST['client'] ?? '');
-            $client_mail = trim($_POST['client_mail'] ?? '');
-            $product = trim($_POST['product'] ?? '');
-            $product_price = trim($_POST['product_price'] ?? '');
-            $address = trim($_POST['address'] ?? '');
-            $date = trim($_POST['date'] ?? '');
-            $delivery_price = trim($_POST['delivery_price'] ?? '');
+    if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+        $courier_name = trim($_POST['name'] ?? '');
+        $client_name = trim($_POST['client'] ?? '');
+        $client_mail = trim($_POST['client_mail'] ?? '');
+        $product = trim($_POST['product'] ?? '');
+        $product_price = trim($_POST['product_price'] ?? '');
+        $address = trim($_POST['address'] ?? '');
+        $date = trim($_POST['date'] ?? '');
+        $delivery_price = trim($_POST['delivery_price'] ?? '');
 
-            $csvFile = 'data.csv';
-            $dataRow = [
-                $courier_name,
-                $client_name,
-                $client_mail,
-                $product,
-                $product_price,
-                $address,
-                $date,
-                $delivery_price
-            ];
+        $csvFile = 'data.csv';
+        $dataRow = [
+            $courier_name,
+            $client_name,
+            $client_mail,
+            $product,
+            $product_price,
+            $address,
+            $date,
+            $delivery_price
+        ];
 
-            if (($file = fopen($csvFile, 'a'))) {
-                fputcsv($file, $dataRow);
-                fclose($file);
-                $message = 'Даныне успешно сохранены';
-            }
-            else {
-                $message = 'Ошибка при сохранении данных';
-            }
-            echo $message;
+        if (($file = fopen($csvFile, 'a'))) {
+            fputcsv($file, $dataRow);
+            fclose($file);
+            $message = 'Даныне успешно сохранены';
         }
-    ?>
+        else {
+            $message = 'Ошибка при сохранении данных';
+        }
+        echo $message;
+    }
+?>
