7b552ecf7c988139e0a8c6ae7fc182fcfac520c7
ilya3052 <ipopadin5@gmail.com>
2025-02-18 22:47:03 +0300

    add: валидация оставшихся полей формы на стороне клиента

--- app/index.js
+++ app/index.js
@@ -1,3 +1,86 @@
+const white_list_email_domains = ["yandex.ru", "yandex.by", "yandex.kz", "ya.ru", "mail.ru", "internet.ru", "list.ru", "bk.ru", "inbox.ru", "vk.com", 
+    "rambler.ru"];
+
+const hasNumber = (str) => {
+    return /\d/.test(str);
+}
+
+const hasLetter = (str) => {
+    return /[a-zA-Z]/.test(str);
+}
+
+const client_name = document.getElementById('client_name');
+client_name.addEventListener('blur', () => {
+    if (client_name.value === "" || hasNumber(client_name.value)) {
+        document.querySelector(".client_name_error").innerText = "Укажите корректное имя";
+    }
+}
+)
+client_name.addEventListener('focus', () => {
+    document.querySelector(".client_name_error").innerText = "";
+}
+)
+
+const courier_name = document.getElementById('courier_name');
+courier_name.addEventListener('blur', () => {
+    if (courier_name.value === "" || hasNumber(courier_name.value)) {
+        document.querySelector(".courier_name_error").innerText = "Укажите корректное имя";
+    }
+}
+)
+courier_name.addEventListener('focus', () => {
+    document.querySelector(".courier_name_error").innerText = "";
+}
+)
+
+const email = document.getElementById('client_mail');
+email.addEventListener('blur', () => {
+    if (email.value === "" || !white_list_email_domains.includes(email.value.split("@")[1])) {
+        document.querySelector(".email_error").innerText = "Укажите корректный адрес электронной почты";
+    }
+}
+)
+email.addEventListener('focus', () => {
+    document.querySelector(".email_error").innerText = "";
+}
+)
+
+const entrance = document.getElementById('entrance');
+entrance.addEventListener('blur', () => {
+    if (entrance.value === "" || !hasLetter(entrance.value)) {
+        document.querySelector(".entrance_error").innerText = "Укажите корректный номер подъезда";
+    }
+}
+)
+entrance.addEventListener('focus', () => {
+    document.querySelector(".entrance_error").innerText = "";
+}
+)
+
+const apartment = document.getElementById('apartment');
+apartment.addEventListener('blur', () => {
+    if (apartment.value === "" || !hasLetter(apartment.value)) {
+        document.querySelector(".apartment_error").innerText = "Укажите корректный номер квартиры";
+    }
+}
+)
+apartment.addEventListener('focus', () => {
+    document.querySelector(".apartment_error").innerText = "";
+}
+)
+
+const floor = document.getElementById('floor');
+floor.addEventListener('blur', () => {
+    if (floor.value === "" || !hasLetter(floor.value)) {
+        document.querySelector(".floor_error").innerText = "Укажите корректный этаж";
+    }
+}
+)
+floor.addEventListener('focus', () => {
+    document.querySelector(".floor_error").innerText = "";
+}
+)
+
 let element = document.getElementById('client_phone');
 let maskOptions = {
     mask: '+7(000)000-00-00',
