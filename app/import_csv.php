879a19163156f2c468c152857814a46216a3deea
ilya3052 <ipopadin5@gmail.com>
2025-03-09 14:41:19 +0300

    add: запросы для добавления данных в основные таблицы и таблицы связей

--- app/import_csv.php
+++ app/import_csv.php
@@ -1,15 +1,34 @@
-<?php 
+<?php
     require 'db.php';
 
-    $file = fopen('new.csv', 'r');
+    $file = fopen('data.csv', 'r');
 
-    fgetcsv($file); // пропуск заголовка если имеется
+    $headers = fgetcsv($file, 1000, ','); // считывание заголовков
 
-    while (($data = fgetcsv($file, 1000, ','))) {
-        $stmt = $pdo->prepare("INSERT INTO users (name, email, age) VALUES (?, ?, ?)");
-        $stmt->execute([$data[0], $data[1], $data[2]]);
+    while (($row = fgetcsv($file, 1000, ','))) {
+        $data = array_combine($headers, $row);
+
+        $stmt = $pdo->prepare("INSERT INTO client (cilent_fullname, client_phonenumber, client_mail) VALUES (?, ?, ?)");
+        $stmt->execute([$data["client_name"], $data["client_phone"], $data["client_mail"]]);
+        $client_id = $pdo->lastInsertId();
+
+        $stmt = $pdo->prepare("INSERT INTO courier (courier_fullname) VALUES (?)");
+        $stmt->execute([$data["courier_name"]]);
+        $courier_id = $pdo->lastInsertId();
+
+        $stmt = $pdo->prepare("INSERT INTO product (product_name, product_price) VALUES (?, ?)");
+        $stmt->execute([$data["product"], $data["product_price"]]);
+        $article = $pdo->lastInsertId();
+
+        $stmt = $pdo->prepare("INSERT INTO delivery (
+                client_id, courier_id, delivery_city, delivery_street, delivery_house, delivery_entrance, delivery_apartment, delivery_floor, delivery_intercome_code, delivery_date, delivery_price
+            ) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)");
+        $stmt->execute([$client_id, $courier_id, $data["city"], $data["street"], $data["house"], $data["entrance"], $data["apartment"], $data["floor"], $data["intercome_code"], $data["delivery_date"], $data["delivery_price"]]);
+        $delivery_number = $pdo->lastInsertId();
+
+        $stmt = $pdo->prepare("INSERT INTO included (product_article, delivery_number) VALUES (?, ?)");
+        $stmt->execute([$article, $delivery_number]);
     }
 
     fclose($file);
-
 ?>
