c222a8a9c8be910135ad1b7fe8191f861b117c51
ilya3052 <ipopadin5@gmail.com>
2025-03-16 19:19:37 +0300

    add: подключение к бд

---
+++ src/core/database.php
@@ -0,0 +1,28 @@
+<?php 
+    namespace App\core;
+
+    use PDO;
+    use PDOException;
+
+    class Database {
+        private static ?PDO $pdo = null;
+
+        public function connect(): PDO {
+            if (self::$pdo == null) {
+                $config = parse_ini_file(__DIR__, '/../../.env');
+                try {
+                    self::$pdo = new PDO(
+                        "mysql:host={$config['DB_HOST']};dbname={$config['DB_NAME']};charset=utf-8",
+                        $config['DB_USER'],
+                        $config['DB_PASS'],
+                        [PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION]
+                    );
+                    self::$pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+                } catch (PDOException $e) {
+                    die("Connection failed: " . $e->getMessage());
+                }
+            }
+            return self::$pdo;
+        }
+    }
+?>
