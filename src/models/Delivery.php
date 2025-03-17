24c4566079b5ff972f9df3644ee7da6ea160b0c1
ilya3052 <ipopadin5@gmail.com>
2025-03-17 12:11:00 +0300

    add: первые функции для добавления данных

--- src/models/Delivery.php
+++ src/models/Delivery.php
@@ -1,24 +1,39 @@
-<?php 
-    namespace App\models;
+<?php
 
-    use App\core\database;
-    use PDO;
+namespace App\models;
 
-    class Delivery {
-        private $pdo;
+use App\core\database;
+use App\models\Client;
+use PDO;
 
-        public function __construct() {
-            $this->pdo = database::connect();
-        }
+require 'Client.php';
 
-        public function getAll() {
-            $stmt = $this->pdo->query("SELECT * FROM delivery");
-            return $stmt->fetchAll(PDO::FETCH_ASSOC);
-        }
+class Delivery
+{
+    private $pdo;
 
-        public function addDelivery() {
+    public function __construct()
+    {
+        $this->pdo = database::connect();
+    }
+
+    public function getAll()
+    {
+        $stmt = $this->pdo->query("SELECT * FROM delivery");
+        return $stmt->fetchAll(PDO::FETCH_ASSOC);
+    }
+
+    public function addDelivery()
+    {
+        if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+            $client = new Client();
+            $client_name = trim($_POST['client_name'] ?? '');
+            $client_phone = str_replace([' ', '(', ')', '-', '+'], '', trim($_POST['client_phone'] ?? ''));
+            $client_mail = trim($_POST['client_mail'] ?? '');
+            $client_id = $client->addClient($client_name, $client_phone, $client_mail);
+            // по очереди добавляем клиента курьра и продукт
             $stmt = $this->pdo->prepare("INSERT INTO delivery (delivery_name, delivery_price) VALUES (:delivery_name, :delivery_price)");
             $stmt->execute();
-        }        
+        }
     }
-?>
+}
