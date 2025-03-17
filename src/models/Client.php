e59dd1b6be6cbf5a2ad6b87815c20b8b7895ca52
ilya3052 <ipopadin5@gmail.com>
2025-03-17 08:59:37 +0300

    add: модель клиента

---
+++ src/models/Client.php
@@ -0,0 +1,31 @@
+<?php 
+    namespace App\models;
+
+    use App\core\database;
+    use PDO;
+
+    class Client  {
+        private $pdo;
+
+        public function __construct() {
+            $this->pdo = database::connect();
+        }
+
+        public function getAll(): array {
+            $stmt = $this->pdo->query("SELECT * FROM client");
+            return $stmt->fetchAll(PDO::FETCH_ASSOC);
+        }
+
+
+        //добавляет клиента в базу
+        public function addClient(string $client_fullname, string $client_phonenumbmer, string $client_mail): int {
+            $stmt = $this->pdo->prepare("INSERT INTO client (client_fullname, client_phonenumber, client_mail) VALUES (:client_fullname, :client_phonenumber, :client_mail)");
+            $stmt->execute([
+                'client_fullname' => htmlspecialchars($client_fullname),
+                'client_phonenumber' => $client_phonenumbmer,
+                'client_mail' => filter_var($client_mail, FILTER_SANITIZE_EMAIL)
+            ]);
+            return $this->pdo->lastInsertId();
+        }        
+    }
+?>
